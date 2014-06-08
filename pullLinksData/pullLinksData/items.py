47de58ae3240f942f6cab6d52d9448491a1af55b
ThisIsAKhan <sadi.khan@gmail.com>
2014-06-08 01:24:49 -0700

    Update items.py

--- pullLinksData/pullLinksData/items.py
+++ pullLinksData/pullLinksData/items.py
@@ -5,7 +5,7 @@
 
 from scrapy.item import Item, Field
 
-class PulllinksdataItem(Item):
+class linkedinDataItem(Item):
     # define the fields for your item here like:
     
     url							= Field()
